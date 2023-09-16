a5bebee44f355ee6a02e0de610892a2155c54d52
JorgeArguello1999 <jorge.arguello1999@gmail.com>
2023-09-15 23:12:50 -0500

    working: On API to consume

---
+++ face_detection.py
@@ -0,0 +1,33 @@
+import face_recognition
+
+def face_compare(ruta_1, ruta_2):
+    # Cargar las dos imágenes de rostros que deseas comparar
+    imagen1 = face_recognition.load_image_file(ruta_1)
+    imagen2 = face_recognition.load_image_file(ruta_2)
+
+    # Codificar los rostros en ambas imágenes
+    # Suponemos que solo hay un rostro en cada imagen
+    codificacion1 = face_recognition.face_encodings(imagen1)[0]  
+    codificacion2 = face_recognition.face_encodings(imagen2)[0]
+
+    # Calcular la distancia euclidiana entre las codificaciones
+    distancia = face_recognition.face_distance([codificacion1], codificacion2)[0]
+
+    print(distancia)
+    # El valor de distancia es un valor entre 0 y 1, donde 0 indica una similitud perfecta
+    # Puedes establecer un umbral para decidir si las imágenes son suficientemente similares
+    umbral = 0.5
+
+    if distancia < umbral:
+        print("Similares")
+        return True
+    else:
+        print("No similares")
+        return False
+
+if __name__ == "__main__":
+    rutas = [
+        input("Ingresa la ruta de la primera imagen: "),
+        input("Ingresa la ruta de la segunda imagen: ")
+    ]
+    face_compare(rutas[0], rutas[1])
--- main.py
+++ main.py
@@ -1,33 +1,21 @@
-import face_recognition
+from fastapi import FastAPI, File, UploadFile
 
-def face_compare(ruta_1, ruta_2):
-    # Cargar las dos imágenes de rostros que deseas comparar
-    imagen1 = face_recognition.load_image_file(ruta_1)
-    imagen2 = face_recognition.load_image_file(ruta_2)
+app = FastAPI()
 
-    # Codificar los rostros en ambas imágenes
-    # Suponemos que solo hay un rostro en cada imagen
-    codificacion1 = face_recognition.face_encodings(imagen1)[0]  
-    codificacion2 = face_recognition.face_encodings(imagen2)[0]
+# Lista para almacenar imágenes en memoria
+imagenes_en_memoria = []
 
-    # Calcular la distancia euclidiana entre las codificaciones
-    distancia = face_recognition.face_distance([codificacion1], codificacion2)[0]
+# Ruta para cargar una imagen y almacenarla en memoria
+@app.post("/uploadimage/")
+async def upload_image(file: UploadFile):
+    # Lee el contenido de la imagen y guárdalo en la lista de imágenes en memoria
+    imagen = await file.read()
+    imagenes_en_memoria.append(imagen)
+    
+    # Devuelve una respuesta indicando que la imagen se ha almacenado en memoria
+    return {"mensaje": "Imagen almacenada en memoria"}
 
-    print(distancia)
-    # El valor de distancia es un valor entre 0 y 1, donde 0 indica una similitud perfecta
-    # Puedes establecer un umbral para decidir si las imágenes son suficientemente similares
-    umbral = 0.5
-
-    if distancia < umbral:
-        print("Similares")
-        return True
-    else:
-        print("No similares")
-        return False
-
-if __name__ == "__main__":
-    rutas = [
-        input("Ingresa la ruta de la primera imagen: "),
-        input("Ingresa la ruta de la segunda imagen: ")
-    ]
-    face_compare(rutas[0], rutas[1])
+# Ruta para obtener una lista de todas las imágenes en memoria
+@app.get("/getimages/")
+async def get_images():
+    return imagenes_en_memoria
