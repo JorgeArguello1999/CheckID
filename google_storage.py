0efcdefc3ede573b7d908b93c9a81da36abd2203
JorgeArguello1999 <jorge.arguello1999@gmail.com>
2023-09-22 08:27:22 -0500

    feat: Separado en modulos tanto para google_storage y tambien para face_detection

---
+++ google_storage.py
@@ -0,0 +1,28 @@
+import os 
+import uuid
+import base64
+
+IMAGEDIR = "./photos/"
+
+def save(images):
+    # Decodificar las imágenes en base64
+    image1_data = base64.b64decode(images.image1)
+    image2_data = base64.b64decode(images.image2)
+
+    # Generar nombres de archivo únicos
+    filename1 = f"{uuid.uuid4()}.jpg"
+    filename2 = f"{uuid.uuid4()}.jpg"
+
+    # Guardar las imágenes en el directorio IMAGEDIR
+    with open(os.path.join(IMAGEDIR, filename1), "wb") as f1, \
+        open(os.path.join(IMAGEDIR, filename2), "wb") as f2:
+        f1.write(image1_data)
+        f2.write(image2_data)
+        
+    # Retornando las rutas de guardado 
+    filename = [
+        os.path.join(IMAGEDIR, filename1),
+        os.path.join(IMAGEDIR, filename2)
+    ]
+
+    return filename 
--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-import os
-import uuid
-import base64
 
 # Import face_detection
 import face_detection
+import google_storage
 
 app = FastAPI()
-IMAGEDIR = "./photos/"
 
 # Modelo para recibir el JSON con las imágenes codificadas en base64
 class ImageData(BaseModel):
@@ -45,30 +42,16 @@ async def help():
 @app.post("/upload/")
 async def create_upload_files(images: ImageData):
     try:
-        # Decodificar las imágenes en base64
-        image1_data = base64.b64decode(images.image1)
-        image2_data = base64.b64decode(images.image2)
-
-        # Generar nombres de archivo únicos
-        filename1 = f"{uuid.uuid4()}.jpg"
-        filename2 = f"{uuid.uuid4()}.jpg"
-
-        # Guardar las imágenes en el directorio IMAGEDIR
-        with open(os.path.join(IMAGEDIR, filename1), "wb") as f1, \
-             open(os.path.join(IMAGEDIR, filename2), "wb") as f2:
-            f1.write(image1_data)
-            f2.write(image2_data)
+        # Guardando las imagenes en el google cloud
+        filename = google_storage.save(images)
 
         # Realizar la comparación de caras
         answer = face_detection.face_compare(
-            os.path.join(IMAGEDIR, filename1),
-            os.path.join(IMAGEDIR, filename2)
+            filename[0],
+            filename[1]
         )
 
-        # Eliminar los archivos temporales
-        os.remove(os.path.join(IMAGEDIR, filename1))
-        os.remove(os.path.join(IMAGEDIR, filename2))
-
+        # Enviando respuesta
         return {
             "euclidean_distance": answer["distance"],
             "answer": answer["answer"]
