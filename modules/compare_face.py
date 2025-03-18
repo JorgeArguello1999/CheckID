01337a73818ee34e35778dd8e740d8a5320cb83f
JorgeArguello1999 <jorge.arguello1999@gmail.com>
2025-03-18 13:33:39 -0500

    feat: compare_face is functional

---
+++ modules/compare_face.py
@@ -0,0 +1,32 @@
+from face_recognition import (
+    load_image_file,
+    face_encodings, 
+    compare_faces
+)
+
+
+def compare_face(image1:str, image2:str) -> bool:
+    """
+    Compare two faces and return True if they are the same person
+    Remember to use only png images
+    image1: str - Path to the first image
+    image2: str - Path to the second
+    """
+
+    # Load the images
+    image1 = load_image_file(image1)
+    image2 = load_image_file(image2)
+
+    # Get the face encodings
+    face_encoding1 = face_encodings(image1)[0]
+    face_encoding2 = face_encodings(image2)[0]
+
+    # Compare the faces
+    return compare_faces([face_encoding1], face_encoding2)[0]
+
+if __name__ == "__main__":
+    DIR = "uploads/"
+    result = compare_face(f"{DIR}photo1.png", f"{DIR}photo1.png")
+    print(result)
+
+
