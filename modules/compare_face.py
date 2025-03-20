4ee625874c707335c74c3a0ee5f7cf71d94f61f0
JorgeArguello1999 <jorge.arguello1999@gmail.com>
2025-03-20 16:59:52 -0500

    feat: didn't work

--- modules/compare_face.py
+++ modules/compare_face.py
@@ -4,6 +4,7 @@ from face_recognition import face_distance
 
 import numpy as np
 import pickle
+import ast
 
 def encode_image(file) -> np.array:
     """
@@ -46,7 +47,39 @@ def compare_face(image1:str, image2:str) -> dict:
         ] 
     }
 
+def compare_binary(image:str, binary:str) -> dict:
+    """
+    Compare the face vs binary data
+    image: str - Image path
+    binary: str - All binary from picture
+    """
+    clean = ast.literal_eval(binary)
+    data = pickle.loads(clean)
+
+    # Load image
+    image_encode = encode_image(image)
+
+    # Distance 
+    distance = face_distance([data], image_encode)[0]
+
+    # Compare the faces
+    is_same = bool(compare_faces([data], image_encode, tolerance=0.6)[0])
+
+    return {
+        "distance": distance,
+        "is_same": is_same
+    }
+
 if __name__ == "__main__":
+    # compare2faces
+    """
     with open("uploads/photo1.png", "rb") as f1, open("uploads/photo1.png", "rb") as f2:
         result = compare_face(f1, f2)
+    print(result)
+    """
+
+    # compare_binary
+    with open('uploads/data.txt', 'r') as data, open('uploads/photo1.png', 'rb') as photo:
+        print(data.read())
+        result = compare_binary(photo, data.read())
     print(result)
